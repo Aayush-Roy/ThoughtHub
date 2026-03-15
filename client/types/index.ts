d04611de4f3e126a2c8dc5acb9eb4abf1c408796
aayush <ar0671362@gmail.com>
2026-03-15 14:28:00 +0530

    fix types folder location

---
+++ client/types/index.ts
@@ -0,0 +1,23 @@
+export interface Comment {
+  id: string;
+  content: string;
+  createdAt: string;
+  author: {
+    id: string;
+    username: string;
+    imageUrl?: string;
+  };
+}
+
+export interface Post {
+  id: string;
+  content: string;
+  createdAt: string;
+  author: {
+    id: string;
+    username: string;
+    imageUrl?: string;
+  };
+  likes: number;
+  commentsCount?: number;
+}
